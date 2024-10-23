19f5a69f7f000171429cf947ba32d6138b13761d
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-10-23 14:49:52 +0900

    음원 관련 페이지, 컨트롤러 테스트

--- src/main/java/com/koreait/exam/springbatch_app_10/app/song/controller/SongController.java
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/song/controller/SongController.java
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.validation.Valid;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/song")
@@ -27,12 +29,12 @@ public class SongController {
     private final SongService songService;
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
-    public String showWrite() {
+    public String showCreate() {
         return "song/create";
     }
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    public String write(@AuthenticationPrincipal MemberContext memberContext, @Valid SongForm songForm) {
+    public String create(@AuthenticationPrincipal MemberContext memberContext, @Valid SongForm songForm) {
         Member author = memberContext.getMember();
         Song song = songService.create(author, songForm.getSubject(), songForm.getContent());
         return "redirect:/song/" + song.getId() + "?msg=" + Ut.url.encode("%d번 음원이 생성되었습니다.".formatted(song.getId()));
