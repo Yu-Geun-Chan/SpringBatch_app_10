26a07dc09c0036e023cc3a7cb0b3c8b286977f5a
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-10-24 10:38:06 +0900

    상품 등록, 상품 수정

--- db/init.sql
+++ db/init.sql
@@ -3,3 +3,13 @@
 DROP DATABASE IF EXISTS SpringBatch_app_10__dev;
 CREATE DATABASE SpringBatch_app_10__dev;
 USE SpringBatch_app_10__dev;
+
+DROP DATABASE IF EXISTS SpringBatch_app_10__test;
+CREATE DATABASE SpringBatch_app_10__test;
+USE SpringBatch_app_10__test;
+
+SHOW TABLES;
+
+SELECT * FROM `member`;
+SELECT * FROM song;
+SELECT * FROM product;
